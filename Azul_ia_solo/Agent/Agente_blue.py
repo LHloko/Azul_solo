ec24508f61cc558b12d60c2fb118aede797357dd
L. Balieiro <89412529+LHloko@users.noreply.github.com>
2024-05-24 12:20:47 -0300

    Add files via upload

---
+++ Azul_ia_solo/Agent/Agente_blue.py
@@ -0,0 +1,22 @@
+"""
+Created on Thu May 23 16:09:11 2024
+
+@author: lbalieiro@lince.lab
+"""
+
+class BlueAgent:
+    def __init__(self):
+
+        pass
+
+    def get_action(self):
+
+        pass
+
+    def update(self):
+
+        pass
+
+    def decay_epsilon(self):
+
+        pass
---
+++ Azul_ia_solo/Azul_solo_v1.py
@@ -0,0 +1,25 @@
+"""
+Created on Mon May 20 19:20:03 2024
+
+@author: lbalieiro@lince.lab
+"""
+# Classes externas
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+import random
+
+# Classes proprias
+import Gym_Env
+
+# __MAIN__
+def main():
+    # inicializa o ambiente
+    env = gym.make("GymEnv/Azul-v1")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
