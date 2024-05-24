8bcb12d2a1635ede7446363c1fb91237873a8716
L. Balieiro <89412529+LHloko@users.noreply.github.com>
2024-05-24 12:33:39 -0300

    Add files via upload

---
+++ Game_V02/setup.py
@@ -0,0 +1,17 @@
+"""
+Created on Mon May 13 22:37:38 2024
+
+@author: lbalieiro@lince.lab
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Game_classes_V02',
+    version='0.1',
+    packages=find_packages(),
+    description='Jogo de mesa Azul versao 02',
+    author='LH_Balieiro',
+    author_email='lbalieiro@lince.lab',
+    install_requires=[],  # Se seu pacote tiver dependências, liste-as aqui
+)
